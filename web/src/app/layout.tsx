2111cb878dafc062363d65b3337820274ba12c3b
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-05-29 11:06:12 -0400

    append file path comment

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,5 @@
+// file: web/src/app/layout.tsx
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,5 @@
+// file: web/src/app/page.tsx
+
 import Image from "next/image";
 
 export default function Home() {
