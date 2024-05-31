4ef778e26981e59e9d7b8e191c955bfb09779d98
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-05-31 07:36:34 -0400

    make the logo a link to root

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
 // file: web/src/app/page.tsx
 
 import Image from "next/image";
+import Link from "next/link";
 import FileUploader from "../components/FileUploader";
 import Footer from "../components/Footer";
 
@@ -8,7 +9,9 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24 bg-black text-white">
       <div className="flex flex-col items-center mb-8">
-        <Image src="/logo.png" alt="Logo" width={150} height={150} />
+        <Link href="/">
+          <Image src="/logo.png" alt="Logo" width={150} height={150} />
+        </Link>
         <h1 className="text-4xl mt-4">Upload File onchain</h1>
         <h3 className="text-lg mt-2">
           Currently supporting Autonomys, Kusama, Paseo and Polkadot networks
